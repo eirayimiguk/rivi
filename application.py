bfb7a411502b4867f96c51339f1c321640667c99
eirayimiguk <eirayimiguk@gmail.com>
2024-05-12 15:20:39 +0900

    feat: display random images

---
+++ application.py
@@ -0,0 +1,21 @@
+import os
+import random
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    image_files = [
+        filename
+        for filename in os.listdir("static/images")
+        if filename.split(".")[-1].lower() in ["jpg", "jpeg", "png"]
+    ]
+
+    random_images = random.sample(image_files, 3)
+    random_image_urls = [
+        url_for("static", filename=f"images/{image}") for image in random_images
+    ]
+    return render_template("index.html", random_image_urls=random_image_urls)
